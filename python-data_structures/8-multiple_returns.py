85f2bebc53d7ae964651294ae90168de51faa97b
Ilnnn <camoreze@gmail.com>
2026-01-19 10:59:24 +0000

    8-multiple_returns.py

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+
+    if sentence == "":
+        first_char = None
+
+    else:
+        first_char = sentence[0]
+
+    return (lenght, first_char)
