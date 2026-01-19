d3165729b988dc8f18e0ed6c2272a9b06724c1f1
Ilnnn <camoreze@gmail.com>
2026-01-19 10:29:50 +0000

    6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    for row in matrix:
+        row_string = ""
+    
+    for number in row:
+        row_string += "{:d}".format(number)
+        
+        if number != row[-1]:
+            row_string += " "
+
+        print (row_string)
