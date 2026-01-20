08f684f34893c7b2e731b0a0b94fc378d72a0584
Ilnnn <camoreze@gmail.com>
2026-01-20 11:47:54 +0000

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_khey = None
+    max_value = float('-inf') 
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+              max_value = value                
+              best_khey = key
+
+        return best_khey
