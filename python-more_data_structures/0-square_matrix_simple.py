c795778ea41cd4528cf158cae6328b0f1fee4280
Ilnnn <camoreze@gmail.com>
2026-01-20 10:02:07 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+
+        for value in row:
+            new_row.append(value * value)
+
+            new_matrix.append(new_row)
+
+    return new_matrix
