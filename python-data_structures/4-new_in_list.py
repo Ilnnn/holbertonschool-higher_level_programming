a6571e1eb4f88e027e82c2a25caa10236e38897a
Ilnnn <camoreze@gmail.com>
2026-01-21 08:33:05 +0000

    ttt

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,10 +4,10 @@ def new_in_list(my_list, idx, element):
     return new_list
     
     if idx < 0:
-        return new_in_list
+        return new_list
 
     if idx >= len(my_list):
-        return new_in_list
+        return new_list
 
     new_list[idx] = element
 
