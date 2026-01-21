b63a0ca2049effbdc9333a029f479c63f0e2bf9b
Ilnnn <camoreze@gmail.com>
2026-01-21 13:10:41 +0000

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    fnlcount = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end= "")
+            fnlcount += 1
+        except IndexError:
+            break
+    print()
+    return fnlcount
