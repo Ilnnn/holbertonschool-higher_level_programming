da8990e6eb15a735f066199440b5adb7d982e805
Ilnnn <camoreze@gmail.com>
2026-01-21 08:58:02 +0000

    ttt

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    result = []
+    result = set()
 
     for element in set_1:
-        if element is set_2:
+        if element is in set_2:
             result.add(element)
 
     return result
