b2f2d1785af29a977b9576b446b0f2cf123bb50a
Ilnnn <camoreze@gmail.com>
2026-01-21 13:49:24 +0000

    2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    fnlcountdwn = 0
+
+    for i in range(x):
+        try:
+            try: 
+                print("{:d}".format(my_list[i]), end="")
+                fnlcountdwn =+ 1
+            except (ValueError, TypeError):
+                pass            
+        except IndexError:
+                break
+    print()
+    return fnlcountdwn
