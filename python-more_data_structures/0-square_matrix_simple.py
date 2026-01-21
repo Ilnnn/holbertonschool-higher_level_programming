09e87f04f17992190912008eb3d0a215c77efa9e
Ilnnn <camoreze@gmail.com>
2026-01-21 08:24:26 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -8,6 +8,6 @@ def square_matrix_simple(matrix=[]):
 
         for num in row:
             new_row.append(num*num)
-            new_matrix.append(new_row)
+        new_matrix.append(new_row)
 
     return new_matrix
