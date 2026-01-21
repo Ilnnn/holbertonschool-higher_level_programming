89ab4f8dffca70d0572270d6a81fce434a75a46c
Ilnnn <camoreze@gmail.com>
2026-01-21 08:41:22 +0000

    ttt

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,7 +7,7 @@ def print_matrix_integer(matrix=[[]]):
         for number in row:
             row_string += "{:d}".format(number)
         
-        if number != row[-1]:
-            row_string += " "
+            if number != row[-1]:
+                row_string += " "
 
         print (row_string)
