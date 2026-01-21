ddd61e0888cab01d23084d6a4118ace7f6eab580
Ilnnn <camoreze@gmail.com>
2026-01-21 08:29:14 +0000

    ttt

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return none
+        return None
 
     if idx >= len(my_list):
-        return none
+        return None
     
     else:
 
