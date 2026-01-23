8fe397c5437d484934ccfc53ba7b39855444d0b3
Ilnnn <camoreze@gmail.com>
2026-01-23 20:12:22 +0000

    first attempt and save of 3-say files

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that prints a name
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints: My name is <first_name> <last_name>
+    """
+    if not isinstance(fu)
