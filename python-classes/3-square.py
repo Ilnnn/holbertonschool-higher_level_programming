ae18f16f15cd3ae8d2f5622ef924c4ad7d4ca963
Ilnnn <camoreze@gmail.com>
2026-01-26 10:23:16 +0000

    correction of 2- and add of 3-

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
 
@@ -8,3 +8,6 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
