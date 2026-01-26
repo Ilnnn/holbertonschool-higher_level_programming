c1e2000865476859ca26ebd425d5e3177c8ce71f
Ilnnn <camoreze@gmail.com>
2026-01-26 10:42:15 +0000

    correction of pycodestyle for the 3-

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 Module that prints a name
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints: My name is <first_name> <last_name>
@@ -12,5 +13,5 @@ def say_my_name(first_name, last_name=""):
 
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
