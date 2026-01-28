1a4e0ed53b5381335ed8ccc8c6cc3fbef628c31c
Ilnnn <camoreze@gmail.com>
2026-01-28 10:13:09 +0000

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,15 +9,14 @@ class Square:
         """Initializes a Square instance."""
         self.size = size
 
-        @property
-        def size(self):
-            """Gets the size of the square."""
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return self.__size
 
-            return (self.__size)
-
-        @size.setter
-        def size(self, value):
-            """
+    @size.setter
+    def size(self, value):
+        """
         Sets the size of the square.
 
         Args:
@@ -27,13 +26,14 @@ class Square:
             TypeError: If value is not an integer.
             ValueError: If value is negative.
         """
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
 
         if value < 0:
             raise ValueError("size must be >= 0")
 
-            self.__size = value
+        self.__size = value
 
-        def area(self):
-            return (self.__size * self.__size)
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
