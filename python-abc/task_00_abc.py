c95a9bf2df407e48859d52e609196635215078b6
Ilnnn <camoreze@gmail.com>
2026-02-03 09:52:42 +0000

    task_00_abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Abstract base class for animals."""
+
+    def sound(self):
+        """Return the sound made by the animal."""
+        pass
+
+    class Dog(Animal):
+        """Dog class inheriting from Animal."""
+
+        def sound(self):
+            return "Bark"
+    
+    class Cat(Animal):
+        """Cat class inheriting from Animal."""
+
+        def sound(self):
+            return "Meow"
