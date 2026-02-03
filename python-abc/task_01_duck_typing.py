b3979e8343ac3643c6d77e75c048bd48ef1faad1
Ilnnn <camoreze@gmail.com>
2026-02-03 09:55:45 +0000

    task_01_duck_typing.py

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """Abstract base class for shapes."""
+
+    def area(self):
+        pass
+    
+    def perimenter(self):
+        pass
