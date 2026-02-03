29ed4d64f895d43095b7bf5ad90ff617fc3fcabf
Ilnnn <camoreze@gmail.com>
2026-02-03 10:45:32 +0000

    task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,17 @@
+class CountedIterator:
+    """Iterator that counts how many items have been fetched."""
+
+    def __init__(self, iterable):
+        self.iterator = inter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.count += 1
+        return item
+
+    def __iter__(self):
+        return self
+    
+    def get_count(self):
+        return self.count
