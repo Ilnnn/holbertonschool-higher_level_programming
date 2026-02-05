77d9cd1acf19b2637ee20be958d1fe1f397d792c
Ilnnn <camoreze@gmail.com>
2026-02-05 10:57:15 +0000

    task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -3,18 +3,18 @@
 class VerboseList(list):
     """A list that prints messages when modified."""
 
-    def append(self, items):
+    def append(self, item):
         super().append(item)
         print(f"Added {item} to the list.")
-    
+
     def extend(self, iterable):
         count = len(iterable)
         super().extend(iterable)
-        print(f"Extented the list with {count} items.")
+        print(f"Extended the list with {count} items.")
 
     def remove(self, item):
         print(f"Removed {item} from the lists.")
-        super().removed(item)
+        super().remove(item)
 
     def pop(self, index=-1):
         item = self[index]
