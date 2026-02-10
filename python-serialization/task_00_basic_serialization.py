ffa2256e710feb50eea61fa5c6f2c516df1af00f
Ilnnn <camoreze@gmail.com>
2026-02-10 09:34:38 +0000

    task_00_basic_serialization.py

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def serialize_and_save_to_file(data, filename):
+
+    """
+    Sérialise un dictionnaire Python et l'enregistre dans un fichier JSON.
+
+    Args:
+        data (dict): Le dictionnaire à sauvegarder.
+        filename (str): Le nom du fichier JSON de sortie.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    """
+    Charge des données JSON depuis un fichier et les reconvertit en dictionnaire Python.
+
+    Args:
+        filename (str): Le nom du fichier JSON à lire.
+
+    Returns:
+        dict: Le dictionnaire Python obtenu après désérialisation.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
