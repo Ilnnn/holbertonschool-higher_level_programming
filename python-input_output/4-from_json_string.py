c1f5d9301ed98494bf5aec7ef921bc6d1fa63849
Ilnnn <camoreze@gmail.com>
2026-02-11 15:12:53 +0000

    4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import json
+"""Return an object represented by a JSON string."""
 
 
 def from_json_string(my_str):
     """Return a Python object represented by a JSON string."""
-    return json.loads(my_str)
+    return __import__("json").loads(my_str)
