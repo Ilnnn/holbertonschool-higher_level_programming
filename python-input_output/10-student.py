09b1c31c6187ef005237bdb464c1664054318666
Ilnnn <camoreze@gmail.com>
2026-02-11 15:36:27 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Student class"""
+"""Student class with optional attrs filter"""
 
 class Student:
     """Class that defines a student."""
@@ -10,16 +10,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Return a dictionary representation of the Student instance.
-
+    def to_json(self, attrs=None):
+        """
+        Return a dictionary representation of the Student instance.
 
         If attrs is a list of strings, only include those attributes.
         Otherwise, include all attributes.
         """
-
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-
             return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
 
         return self.__dict__.copy()
