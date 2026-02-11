6b4dcb08db5f7f301dd7ded43ce199aeafe80794
Ilnnn <camoreze@gmail.com>
2026-02-11 15:17:09 +0000

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
+"""Write an object to a text file using JSON representation."""
 
 
 def load_from_json_file(filename):
     """Create an object from a JSON file."""
     with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+        __import__("json").dump(my_obj, f)
