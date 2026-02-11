3a7cfa412993be0ce00fc629024a2732761c9444
Ilnnn <camoreze@gmail.com>
2026-02-11 15:07:30 +0000

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Append string to UTF8 file."""
 
-
 def append_write(filename="", text=""):
-    """App string to end of UTF-8 text file, return the numb of char added."""
+    """Append a string to the end of a UTF-8 text file and return number of characters added."""
     with open(filename, "a", encoding="utf-8") as f:
-        nb_chars = f.write(text)
-    return f.write(text)
+        return f.write(text)
