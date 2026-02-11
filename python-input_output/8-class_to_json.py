93a52657ed44c96f4a5d260040b79c3f34c772df
Ilnnn <camoreze@gmail.com>
2026-02-11 15:29:37 +0000

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""JSON serialization utility."""
+
+
 def class_to_json(obj):
-    """Return dictionary description of an object for JSON serialization."""
+    """Return dictio description of an object for JSON serialization."""
     return obj.__dict__
