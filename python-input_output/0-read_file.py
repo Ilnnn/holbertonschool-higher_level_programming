8cb1ba90389cc9e06dfda8e9a52676799e8f248d
Ilnnn <camoreze@gmail.com>
2026-02-11 14:55:37 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Read UTF8 file to stdout."""
+
+
 def read_file(filename=""):
     """Read a UTF-8 text file and print its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
