dbe627f46732c00e50bb010ae1002e3683783707
Ilan <camoreze@gmail.com>
2026-03-04 16:14:58 +0100

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Usin the hbtn_0d_tvshows db in school project
-SHOW GRANT FOR 'user_0d_1' @'localhost'; 
-SHOW GRANT FOR 'user_0d_2' @'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost'; 
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
