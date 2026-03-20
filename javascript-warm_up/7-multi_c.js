9dbec52d9f98a94a279040df9adfd6e80ccbbd6c
Ilnnn <camoreze@gmail.com>
2026-03-20 10:58:43 +0000

    7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const count = parseInt(process.argv[2]);
+
+if (isNaN(count)) 
+{
+  console.log('Missing number of occurrences');
+} else 
+{
+  for (let i = 0; i < count; i++) 
+  {
+    console.log('C is fun');
+  }
+}
