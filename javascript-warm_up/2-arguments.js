de96d41724620879bbde83a055385e379075584d
Ilnnn <camoreze@gmail.com>
2026-03-20 10:42:31 +0000

    tt

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,12 @@
 
 const count = process.argv.length - 2;
 
-if (count === 0) {
+if (count === 0) 
+{
   console.log('No argument');
 } else if (count === 1) {
   console.log('Argument found');
-} else {
+} else 
+{
   console.log('Arguments found');
 }
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 const firstArg = process.argv[2];
 
-if (firstArg === undefined) {
+if (firstArg === undefined) 
+{
   console.log('No argument');
-} else {
+} else 
+{
   console.log(firstArg);
 }
