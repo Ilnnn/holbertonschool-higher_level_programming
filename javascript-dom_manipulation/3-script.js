c28da481896a1eec313ff2f8469b2d7adb4681d9
Ilnnn <camoreze@gmail.com>
2026-03-23 10:02:06 +0000

    T0 to T3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.querySelector('#toggle_header');
+const header = document.querySelector('header');
+
+toggleBtn.addEventListener('click', () => 
+{
+  if (header.classList.contains('red')) 
+  {
+    header.classList.remove('red');
+    header.classList.add('green');
+  } 
+  else 
+  {
+    header.classList.remove('green');
+    header.classList.add('red');
+  }
+});
