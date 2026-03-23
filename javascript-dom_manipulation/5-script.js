6148b9d8118dd444fc896212d6d3f411492b80a9
Ilnnn <camoreze@gmail.com>
2026-03-23 10:37:09 +0000

    T4 and T5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+const updateBtn = document.querySelector('#update_header');
+const header = document.querySelector('header');
+
+updateBtn.addEventListener('click', () =>
+{
+    header.textContent = 'New Header!!!';
+});
